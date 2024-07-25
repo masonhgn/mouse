78a73acd89bab12d1f91ef99affd61229d6afbc4
Mason Hagan <mason24h@gmail.com>
2024-07-24 21:39:09 -0400

    updated client adding client ids

--- Client.py
+++ Client.py
@@ -1,14 +1,23 @@
+import uuid
+
+
 class Client:
-    def __init__(self, client_id, balance):
-        self.client_id = client_id
+    def __init__(self, balance):
+        self.client_id = str(uuid.uuid4())
         self.balance = balance
-        self.portfolio = {}  # Dictionary to hold ticker and quantity
+        self.portfolio = {}
+    
+
 
     def update_portfolio(self, ticker, quantity):
         if ticker in self.portfolio:
             self.portfolio[ticker] += quantity
         else:
             self.portfolio[ticker] = quantity
+
+        #if we sold all of a security, let's make sure we remove it from our portfolio
+        if self.portfolio[ticker] == 0: del self.portfolio[ticker]
+
 
     def update_balance(self, amount):
         self.balance += amount
